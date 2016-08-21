75de47cfe3450e61dad2197214903c3043377149
johnoneil <oneil.john@gmail.com>
2016-08-20 17:39:22 -0700

    Added vimrc and fixed user prompt.

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 BASHRC=.bashrc
+VIMRC=.vimrc
 
 #install our current bashrc
 cp ${BASHRC} ${HOME}/
-. ${HOME}/${BASHRC}
+
+#install our .vimrc
+cp ${VIMRC} ${HOME}/
---
+++ testfile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "emscripten.h"
+
+class Person: public entity
+{
+:public
+  int getsomevalue(const arg& 1, const arg* 2)
+  {
+    return null;
+  }
+
+
+}
+
+
+int main(int argc, char* argv[])
+{
+  exit(-1);
+}
