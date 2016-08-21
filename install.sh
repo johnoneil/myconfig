3cf5a168a5274f15cc964f6d47a09052bd5d924c
johnoneil <oneil.john@gmail.com>
2016-08-20 19:06:10 -0700

    Added dircolors management.

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 BASHRC=.bashrc
 VIMRC=.vimrc
+DIRCOLORS=.dircolors
 
 #install our current bashrc
 cp ${BASHRC} ${HOME}/
 
 #install our .vimrc
 cp ${VIMRC} ${HOME}/
+
+#install our .dircolors
+cp ${DIRCOLORS} ${HOME}/
